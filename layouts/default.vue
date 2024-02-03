76ede8a6cc11277d9de893854f97f53b423f308d
rxon <eric100son@gmail.com>
2024-02-03 04:57:59 +0300

    installation

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    Default layout
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Loginnnnnnnnn</div>
+</template>
+
+<script setup lang="ts"></script>
